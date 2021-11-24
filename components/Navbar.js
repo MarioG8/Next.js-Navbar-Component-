f81c9d322de5eb14e8673ca4b3f2d5588fa9cc30
MarioG8 <golikmariusz7@gmail.com>
2021-11-24 20:10:58 +0100

    added new project navbar next component

---
+++ components/Navbar.js
@@ -0,0 +1,87 @@
+import React, { useState, useRef, useEffect } from "react";
+import { VscMenu } from "react-icons/vsc";
+import { GiFootprint, GiNinjaHead } from "react-icons/gi";
+import { links } from "../data/data.js";
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  const [navigation, setNavigation] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY >= 750) {
+        setNavigation(true);
+      } else {
+        setNavigation(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
+  return (
+    <header
+      className={
+        navigation
+          ? `${styles.navigation} ${styles.active}`
+          : `${styles.navigation}`
+      }
+    >
+      <nav className={styles.nav}>
+        <div className={styles.center}>
+          <div className={styles.header}>
+            <div className={styles.logo}>
+              <Link href="/">
+                <a>navigation</a>
+              </Link>
+              <GiNinjaHead className={styles.icon} />
+            </div>
+
+            <button className={styles.toggle} onClick={toggleLinks}>
+              <VscMenu size={30} />
+            </button>
+          </div>
+          <div className={styles.linksContainer} ref={linksContainerRef}>
+            <ul className={styles.links} ref={linksRef}>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id} onClick={toggleLinks}>
+                    <Link href={url}>
+                      <a>{text}</a>
+                    </Link>
+                  </li>
+                );
+              })}
+              <div className={styles.login_container}>
+                <Link href="/login" passHref>
+                  <button className={styles.login_btn}>Log in</button>
+                </Link>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ data/data.js
@@ -0,0 +1,58 @@
+import {
+  FaInstagram,
+  FaFacebookSquare,
+  FaYoutube,
+  FaTwitter,
+} from "react-icons/fa";
+import { GiFootprint } from "react-icons/gi";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/services",
+    text: "services",
+  },
+  {
+    id: 3,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "/",
+    icon: <GiFootprint />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.instagram.com",
+    icon: <FaInstagram />,
+  },
+  {
+    id: 4,
+    url: "https://www.youtube.com",
+    icon: <FaYoutube />,
+  },
+  {
+    id: 5,
+    url: "https://www.facebook.com",
+    icon: <FaFacebookSquare />,
+  },
+];
